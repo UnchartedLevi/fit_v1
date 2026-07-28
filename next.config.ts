c5e98f115607ae59926d1a2f1794c124d6c82ac7
UnchartedLevi <saint.levi777@gmail.com>
2026-07-28 11:31:10 +0100

    Redirect production aliases to custom domain

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,22 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async redirects() {
+    return [
+      {
+        source: "/:path*",
+        has: [{ type: "host", value: "www.fits4l.xyz" }],
+        destination: "https://fits4l.xyz/:path*",
+        permanent: true,
+      },
+      {
+        source: "/:path*",
+        has: [{ type: "host", value: "fit-v1.vercel.app" }],
+        destination: "https://fits4l.xyz/:path*",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
